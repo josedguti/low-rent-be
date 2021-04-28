f402aeb3a1ea8a58b5b3e6356010514e3c75c8fd
YOUR NAME <YOUR@EMAIL.com>
2021-04-28 15:28:44 -0400

    moved wishlist.rb from concerns

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -3,4 +3,9 @@ class WishlistsController < ApplicationController
         wishlist = Wishlist.all
         render json: {status: 200, wishlist: wishlist}
     end
+
+    def show 
+        wishlist = Wishlist.find(params[:id])
+        render json: {status: 200, wishlist: wishlist}
+    end
 end
