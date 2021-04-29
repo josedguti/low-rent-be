9d4fe367dee4a45b8a4f90941949530e27f0bd94
Jessica Paul <paul.jessica95@gmail.com>
2021-04-29 11:39:03 -0400

    add data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Clothe.create([{
+    name: 'Black Shirt',
+    category: 'Mens',
+    price: 10.99,
+    picture: '',
+    description: 'Random Blackshirt that you can use for any casual ocassion',
+}, {
+    name: 'White Shirt',
+    category: 'Mens',
+    price: 10.99,
+    picture: '',
+    description: 'The white shirt is the ideal multitasker, the ultimate in simplicity and versatility',
+}, {
+    name: 'Blue Pants',
+    category: 'Mens',
+    price: 19.99,
+    picture: '',
+    description: 'Good looking pants that you can use to have dinner with friends',
+}, {
+    name: 'Black Pants',
+    category: 'Mens',
+    price: 19.99,
+    picture: '',
+    description: 'Pretty nice pants to use for any ocassion',
+}, {
+    name: 'Pink Shirt',
+    category: 'Womens',
+    price: 10.99,
+    picture: '',
+    description: 'Random Pinkshirt that you can use for any casual ocassion',
+}, {
+    name: 'Black Skirt',
+    category: 'Womens',
+    price: 12.99,
+    picture: '',
+    description: 'Professional skirt you could use for work or lunch',
+}, {
+    name: 'Yellow Blouse',
+    category: 'Womens',
+    price: 19.99,
+    picture: '',
+    description: 'Very stylish yellow blouse to go out for dinner',
+}, {
+    name: 'Blue Jeans',
+    category: 'Womens',
+    price: 19.99,
+    picture: '',
+    description: 'Pretty nice blue jeans to use for any ocassion',
+}])
